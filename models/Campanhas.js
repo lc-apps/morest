2f24a5bc895dfe23585faa7e03d8faae73e12184
lc-apps <carlos_hpc@hotmail.com>
2017-09-05 12:35:40 -0300

    Update Campanhas.js

--- models/Campanhas.js
+++ models/Campanhas.js
@@ -2,6 +2,8 @@ var db=require('../dbconnection'); //reference of dbconnection.js
 
 var Campanhas={
 
+// normal
+ /*
 getAllCampanhas:function(callback){
 
 
@@ -12,6 +14,24 @@ return db.query("SELECT * FROM campanhas_produtos" +
                 " AND campanhas.data_fim >= CURDATE()"+
                 " AND campanhas.status =1 "+
                 " AND campanhas_produtos.status =1 ",callback);
+
+},
+*/
+// seleciona agrupado
+getAllCampanhas:function(callback){
+return db.query("SELECT*, MIN(preco_por) as de,"+
+              "max(preco_por) as 'até',"+
+              "count(produtos.id) as ofertas,"+
+              "count(idestabelecimento) as mercados"+
+              "FROM campanhas_produtos"+
+              "INNER JOIN campanhas ON campanhas.id = campanhas_produtos.idcampanha"+
+              "INNER JOIN produtos ON produtos.id = campanhas_produtos.idproduto"+
+              "WHERE campanhas.data_inicio <= CURDATE( ) "+
+              "AND campanhas.data_fim >= CURDATE( ) "+
+              "AND campanhas.status =1"+
+              "AND campanhas_produtos.status =1"+
+              "group by produtos.id.status =1"+
+              "group by produtos.id ",callback);
 
 },
 
